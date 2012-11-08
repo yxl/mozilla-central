1eabd50c9bb92999c9c2fb43b25688a76b7b9b26
Ethan Hugg <ethanhugg@gmail.com>
2012-11-08 11:18:13 -0800

    Debug patch - fix logging to all go through CSF

--- media/webrtc/signaling/src/sipcc/cpr/linux/cpr_linux_ipc.c
+++ media/webrtc/signaling/src/sipcc/cpr/linux/cpr_linux_ipc.c
@@ -243,7 +243,7 @@ cprCreateMessageQueue (const char *name, uint16_t depth)
      * Find a unique key
      */
     key = ftok("/proc/self", key_id++);
-    printf("key = %x\n", key);
+    CSFLogDebug(logTag, "key = %x\n", key);
 
     if (key == -1) {
         CPR_ERROR("%s: Key generation failed: %d\n", fname, errno);
@@ -257,7 +257,7 @@ cprCreateMessageQueue (const char *name, uint16_t depth)
     msgq->queueId = msgget(key, (IPC_EXCL | IPC_CREAT | 0666));
     if (msgq->queueId == -1) {
         if (errno == EEXIST) {
-            printf("Q exists so first remove it and then create again\n");
+            CSFLogDebug(logTag, "Q exists so first remove it and then create again\n");
                 /* Remove message queue */
             msgq->queueId = msgget(key, (IPC_CREAT | 0666));
             if (msgctl(msgq->queueId, IPC_RMID, &buf) == -1) {
@@ -270,7 +270,7 @@ cprCreateMessageQueue (const char *name, uint16_t depth)
             msgq->queueId = msgget(key, (IPC_CREAT | 0666));
         }
     } else {
-        printf("there was no preexisting q..\n");
+        CSFLogDebug(logTag, "there was no preexisting q..\n");
 
     }
 
@@ -285,7 +285,7 @@ cprCreateMessageQueue (const char *name, uint16_t depth)
         cpr_free(msgq);
         return NULL;
     }
-    printf("create message q with id=%x\n", msgq->queueId);
+    CSFLogDebug(logTag, "create message q with id=%x\n", msgq->queueId);
 
     /* flush the q before ?? */
 
@@ -350,7 +350,7 @@ cprDestroyMessageQueue (cprMsgQueue_t msgQueue)
     cpr_msg_queue_t *msgq;
     void *msg;
     struct msqid_ds buf;
-    printf("Destroy message Q called..\n");
+    CSFLogDebug(logTag, "Destroy message Q called..\n");
 
 
     msgq = (cpr_msg_queue_t *) msgQueue;
@@ -778,6 +778,7 @@ cprPostMessage (cpr_msg_queue_t *msgq, void *msg, void **ppUserData)
     		 sizeof(struct msgbuffer) - offsetof(struct msgbuffer, msgPtr),
                IPC_NOWAIT) != -1) {
         msgq->currentCount++;
+        CSFLogDebug(logTag, "Success sending to message queue %d", msgq->queueId);
         return CPR_MSGQ_POST_SUCCESS;
     } else {
         CSFLogError(logTag, "Error sending to message queue %d : %p : %d",
