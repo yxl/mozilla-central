05f67130667291ef649b63f781ec3fccb8238ab6
Yuan Xulei(袁徐磊) <xyuan@mozilla.com>
2013-11-01 19:17:11 +0800

    WIP Part1 v4: WebIDL and building.

---
+++ dom/promise/AbortableProgressPromise.h
@@ -0,0 +1,50 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_AbortableProgressPromise_h__
+#define mozilla_dom_AbortableProgressPromise_h__
+
+#include "nsAutoPtr.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsWrapperCache.h"
+
+#include "mozilla/Attributes.h"
+#include "mozilla/ErrorResult.h"
+
+struct JSContext;
+
+namespace mozilla {
+namespace dom {
+
+class VoidAnyCallback;
+
+class AbortableProgressPromise MOZ_FINAL : public nsISupports /* Change nativeOwnership in the binding configuration if you don't want this */,
+                                           public nsWrapperCache /* Change wrapperCache in the binding configuration if you don't want this */
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(AbortableProgressPromise)
+
+public:
+  AbortableProgressPromise();
+
+  ~AbortableProgressPromise();
+
+  // TODO: return something sensible here, and change the return type
+  AbortableProgressPromise* GetParentObject() const;
+
+  virtual JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  void Abort();
+
+  // Mark this as resultNotAddRefed to return raw pointers
+  already_AddRefed<AbortableProgressPromise> Progress(VoidAnyCallback& callback);
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_AbortableProgressPromise_h__
